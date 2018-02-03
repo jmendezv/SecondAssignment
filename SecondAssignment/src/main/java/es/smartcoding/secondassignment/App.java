4794f2527ecdb0064aa903d68bf0ba09d6cee00f
Pep <jmendez1@xtec.cat>
2018-02-03 21:07:32 +0100

    sum method documented

--- SecondAssignment/src/main/java/es/smartcoding/secondassignment/App.java
+++ SecondAssignment/src/main/java/es/smartcoding/secondassignment/App.java
@@ -6,5 +6,12 @@ package es.smartcoding.secondassignment;
  *
  */
 public interface App {
+    /**
+     * 
+     * @param a
+     * @param b
+     * @return la suma de a y b
+     * @throws Exception
+     */
     public abstract int sum(final int a, final int b) throws Exception;
 }
