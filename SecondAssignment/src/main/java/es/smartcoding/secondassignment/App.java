6c5143cbc0b96e5da9fb0caa626fa1b668f64c27
Pep <jmendez1@xtec.cat>
2018-02-03 21:01:25 +0100

    First commit

---
+++ SecondAssignment/src/main/java/es/smartcoding/secondassignment/App.java
@@ -0,0 +1,10 @@
+package es.smartcoding.secondassignment;
+
+/**
+ * 
+ * @author pep
+ *
+ */
+public interface App {
+    public abstract int sum(final int a, final int b) throws Exception;
+}
---
+++ SecondAssignment/src/main/java/es/smartcoding/secondassignment/AppImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package es.smartcoding.secondassignment;
+
+/**
+ * @author pep
+ *
+ */
+public class AppImpl implements App {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see es.smartcoding.secondassignment.App#sum(int, int)
+     */
+    @Override
+    public int sum(int a, int b) throws Exception {
+
+	return a + b;
+    }
+
+}
